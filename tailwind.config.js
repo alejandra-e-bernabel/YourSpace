dc28acf68c31fff6a7c46d6ab5d4e88f999186c7
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-10-05 15:04:23 -0400

    added custom colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
+    colors: {
+      'white': '',
+      'blue': '#CEEAF7',
+      'violet': '#4B3269',
+      'mauve': '#B55A5F',
+      'black': '#0B0E14',
+      'cadet': '#1D1D46',
+    },
     extend: {},
   },
   plugins: [
