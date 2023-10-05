fa70c971d1f9f079e80ad1279a33a26d1437a38f
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-10-05 15:15:11 -0400

    fixed cust colors, changed border colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   theme: {
     colors: {
       'white': '',
-      'blue': '#CEEAF7',
-      'violet': '#4B3269',
+      'bluey': '#CEEAF7',
+      'violent': '#4B3269',
       'mauve': '#B55A5F',
-      'black': '#0B0E14',
+      'rblack': '#0B0E14',
       'cadet': '#1D1D46',
     },
     extend: {},
