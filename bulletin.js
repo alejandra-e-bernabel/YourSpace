0e21eca074f60f1369b139442ceccd2e7c41618b
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-10-09 18:10:15 -0400

    amended js but still needs functionality

--- bulletin.js
+++ bulletin.js
@@ -3,38 +3,29 @@ let posts =JSON.parse(localStorage.getItem('posts')) || [];
 
 //function to render posts on the bulletin board
 function renderPosts()  {
-    const bulletinBoard = document.getElementById('bulletin-board');
-
-    // bulletinBoard.innerHTML = '';
-    // posts.forEach((post, index) =>  {
-    //     const postElement = document.createElement('div');
-    //     postElement.className = 'bg-white p-4 rounded shadow';
-    //     postElement.innerHTML = `
-    //     <h2 class="text-xl font-semibold mb-2">${post.title}</h2>
-    //     <p>${post.content}</p>
-    //     <button class="mt-4 bg-red-500 text-white px-4 py-2 rounded" onclick="deletePost(${index})">Delete</button>
-    //     `;
-    //     bulletinBoard.appendChild(postElement);
-    // });
+    const bulletinBoard = document.getElementsByClassName('bulletin-board');
+    for(let i = 0; i < bulletinBoard.length; i++)   {
+        bulletinBoard[i].value = posts[i] || '';
+    }
 }
-function saveEvent()    {
-    $('.saveBtn').on('click', function ()   {
-     
-    })
-}
-
-//function to save posts to local storage
-function savePost(title, content)   {
-    posts.push({ title, content });
-    localStorage.setItem('posts', JSON.stringify(posts));
-    renderPosts();
+//saves post to local storage
+function saveEvent() {
+    const saveButtons = document.getElementsByClassName('saveBtn');
+    for (let i = 0; i < saveButtons.length; i++) {
+        saveButtons[i].addEventListener('click', function () {
+            const key = i.toString(); // Using index as the key
+            const value = document.getElementsByClassName('bulletin-board')[i].value;
+            posts[i] = value; // Update posts array
+            localStorage.setItem('posts', JSON.stringify(posts)); // Save posts array to local storage
+        });
+    }
 }
 
 //function to delete a post
-function deletePost(index)  {
+function deletePost(index) {
     posts.splice(index, 1);
     localStorage.setItem('posts', JSON.stringify(posts));
     renderPosts();
 }
-
+saveEvent();
 renderPosts();
