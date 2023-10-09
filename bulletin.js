798e164c778ad4aeb41ce1af9b47643933771342
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-10-09 17:10:53 -0400

    added text area to bulletin board

--- bulletin.js
+++ bulletin.js
@@ -4,18 +4,25 @@ let posts =JSON.parse(localStorage.getItem('posts')) || [];
 //function to render posts on the bulletin board
 function renderPosts()  {
     const bulletinBoard = document.getElementById('bulletin-board');
-    bulletinBoard.innerHTML = '';
-    posts.forEach((post, index) =>  {
-        const postElement = document.createElement('div');
-        postElement.className = 'bg-white p-4 rounded shadow';
-        postElement.innerHTML = `
-        <h2 class="text-xl font-semibold mb-2">${post.title}</h2>
-        <p>${post.content}</p>
-        <button class="mt-4 bg-red-500 text-white px-4 py-2 rounded" onclick="deletePost(${index})">Delete</button>
-        `;
-        bulletinBoard.appendChild(postElement);
-    });
+
+    // bulletinBoard.innerHTML = '';
+    // posts.forEach((post, index) =>  {
+    //     const postElement = document.createElement('div');
+    //     postElement.className = 'bg-white p-4 rounded shadow';
+    //     postElement.innerHTML = `
+    //     <h2 class="text-xl font-semibold mb-2">${post.title}</h2>
+    //     <p>${post.content}</p>
+    //     <button class="mt-4 bg-red-500 text-white px-4 py-2 rounded" onclick="deletePost(${index})">Delete</button>
+    //     `;
+    //     bulletinBoard.appendChild(postElement);
+    // });
 }
+function saveEvent()    {
+    $('.saveBtn').on('click', function ()   {
+     
+    })
+}
+
 //function to save posts to local storage
 function savePost(title, content)   {
     posts.push({ title, content });
