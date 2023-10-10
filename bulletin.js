61d93650603027741159f62b0921dd0b6558f3fd
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-10-10 14:34:41 -0400

    created local storage and it functions relatively okay

--- bulletin.js
+++ bulletin.js
@@ -1,31 +1,31 @@
-//load posts from local storage
-let posts =JSON.parse(localStorage.getItem('posts')) || [];
-
-//function to render posts on the bulletin board
-function renderPosts()  {
-    const bulletinBoard = document.getElementsByClassName('bulletin-board');
-    for(let i = 0; i < bulletinBoard.length; i++)   {
-        bulletinBoard[i].value = posts[i] || '';
-    }
-}
-//saves post to local storage
-function saveEvent() {
-    const saveButtons = document.getElementsByClassName('saveBtn');
-    for (let i = 0; i < saveButtons.length; i++) {
-        saveButtons[i].addEventListener('click', function () {
-            const key = i.toString(); // Using index as the key
-            const value = document.getElementsByClassName('bulletin-board')[i].value;
-            posts[i] = value; // Update posts array
-            localStorage.setItem('posts', JSON.stringify(posts)); // Save posts array to local storage
+$(document).ready(function () {
+    //event listener on save buttons
+    //this will aloow you to save events to local storage
+    function saveEvent() {
+        $('#saveBtn').on('click', function () {
+            let key = $(this).parent().attr('id');
+            let value = $(this).siblings('.bulletin-board').val();
+            localStorage.setItem(key, value);
         });
     }
-}
+    // this function will get any user input that was saved to LS and set the values of the corrosponding text area elems
+    function loadEvent() {
+        $('.post-container').each(function () {
+            const key = $(this).attr('id');
+            const value = localStorage.getItem(key);
+            $(this).children('.bulletin-board').val(value);
+        });
+    }
+// this will erase the text areas when the delete button is clicked
+    $('.post-container').on('click', '#deleteBtn', function () {
+        let key = $(this).parent().attr('id');
+        localStorage.removeItem(key);
+        $(this).siblings('.bulletin-board').val('');
+    });
 
-//function to delete a post
-function deletePost(index) {
-    posts.splice(index, 1);
-    localStorage.setItem('posts', JSON.stringify(posts));
-    renderPosts();
-}
-saveEvent();
-renderPosts();
+
+  
+    saveEvent();
+    loadEvent();
+});
+
