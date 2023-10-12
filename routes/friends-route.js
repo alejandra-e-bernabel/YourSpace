c7b434e14f472c86a54f8d211cd00d91f0873406
Nicole Torres <nicolejoan9@yahoo.com>
2023-10-11 20:12:24 -0400

    got server to run and database to sync as well as seeds to sync , as well as avatars showing via http requests

--- routes/friends-route.js
+++ routes/friends-route.js
@@ -1,74 +1,82 @@
 const router = require('express').Router();
 const Friend = require("../models/friends-model.js");
 
-router.get('/friend/:id', (req, res) => {
-   Friend.findByPk({
-     where: {
-       id: req.params.id,
-     },
-   })
-     .then((friend) => {
-       res.json(friend);
-     })
-     .catch((error) => {
-       console.log(error);
-       return res.status(404).send('Friend not found');
-     });
 
+router.get("/friend/:id", (req, res) => {
+  Friend.findByPk(req.params.id)
+    .then((friend) => {
+      if (!friend) {
+        return res.status(404).send("Friend not found");
+      }
+
+      if (!friend.image_path) {
+        return res.status(404).send("Image not found for this friend");
+      }
+
+      res.setHeader("Content-Type", "image/jpeg");
+
+   
+      const imagePath = friend.image_path;
+      res.sendFile(imagePath, { root: "./" });
+
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).send("Internal Server Error");
+    });
 });
-
-// const imageUrl = /images/Friend.image_path;
-// const html = <img src="${imageUrl}" alt="Friend Image"/>;
-// res.send(html);
-// .then((friend) => {
-//     res.status(200).json(friend);
-//   }).catch((error) => {
-//     console.error('Unable to fetch friend:', error);
-//     res.status(500).send('Error fetching friend');
-//   });
-
-// const imageUrl = "/images/Friend.image_path";
-// const html = <img src="${imageUrl}" alt="Friend Image"/>;
-
-// res.send(html);
-
-// then((friend) => {
-//   res.status(200).json(friend);
-// }).catch((error) => {
-//   console.error('Unable to fetch friend:', error);
-//   res.status(500).send('Error fetching friend');
-// });
-
-
-
-
-
 
 router.put('/:id', (req, res) => {
- Friend.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-     }).then((people) => {
-    res.status(200).json(people);
-  }).catch((error) => {    
-    console.log(error);
-    res.status(400).json(error);
-  })
+   Friend.update(req.params.id)
+    .then((friend) => {
+      if (!friend) {
+        return res.status(404).send("Friend not found");
+      }
+
+      if (!friend.image_path) {
+        return res.status(404).send("Image not found for this friend");
+      }
+
+      res.setHeader("Content-Type", "image/jpeg");
+
+   
+      const imagePath = friend.image_path;
+      res.sendFile(imagePath, { root: "./" });
+
+    }).then((friend) => {
+    res.status(200).json(friend);
+    })
+    .catch((error) => {
+      console.log(error);
+       res.status(400).json(error);
+      return res.status(500).send("Internal Server Error");
+    });
 });
 
-router.delete('/:id', (req, res) => {
-   Friend.destroy({
-     where: {
-       id: req.params.id,
-     },
-   }).then((people) => {
-    res.json(people);
-     }).catch((error) => {    
-    console.log(error);
-    res.status(400).json(error);
-  })
- 
+router.delete("/:id", (req, res) => {
+  Friend.destroy(req.params.id)
+    .then((friend) => {
+      if (!friend) {
+        return res.status(404).send("Friend not found");
+      }
+
+      if (!friend.image_path) {
+        return res.status(404).send("Image not found for this friend");
+      }
+
+      res.setHeader("Content-Type", "image/jpeg");
+
+      const imagePath = friend.image_path;
+      res.sendFile(imagePath, { root: "./" });
+    })
+    .then((friend) => {
+      res.status(200).json(friend);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json(error);
+      return res.status(500).send("Internal Server Error");
+    });
 });
 
 
