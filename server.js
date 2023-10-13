439c4787479caaf022ade7972d97eb180beef87a
Alejandra <alejandra.e.bernabel@gmail.com>
2023-10-13 12:26:42 -0400

    All server files are now merged and labeled

--- server.js
+++ server.js
@@ -6,6 +6,37 @@ const sequelize = require('./config/connection'); // Import the Sequelize connec
 const { Song, User, Playlist, Playlist_song } = require('./models'); // Import your models
 const userController = require('./controllers/user-controller'); // Import the user controller
 
+// *******************YANA ROUTES AND DEPENDENCIES
+require('dotenv').config();
+const exphbs = require('express-handlebars');
+const routes = require('./routes');
+
+// const { User } = require('./models');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// *******************END OF YANA's ROUTES AND DEPENDENCIES
+
+
+//******************* NICOLE's ROUTES AND DEPENDENCIES
+const routes = require("./routes/friends-route.js");
+const Friends = require("./models/friends-model");
+
+//****************** END OF NICOLE's ROUTESS AND DEPENDENCIES
+
+//******************* ALEJANDRA's ROUTES AND DEPENDENCIES
+
+const fs = require('fs');
+const getTimeAndDate = require ("./public/assets/js/timeandDate.js");
+const getLikesandDislikes = require("./public/assets/js/likesAndDislikes.js");
+
+//unique ID generator
+const { v4: uuidv4 } = require('uuid');
+// call using uuidv4();
+
+//******************* END OF ALEJANDRA's ROUTES AND DEPENDENCIES
+
+
 // Import your route modules
 const songRoutes = require('./routes/song-routes');
 const userRoutes = require('./routes/user-routes');
@@ -14,6 +45,49 @@ const playlistSongRoutes = require('./routes/playlist_song-routes');
 
 const app = express();
 const port = process.env.PORT || 3001; // Use the PORT from environment variables or default to 3001
+
+// ***********************YANA MIDDLEWARE
+
+// Template Engine Middleware
+const hbs = exphbs.create({});
+
+// Session Middleware
+const sess = {
+  secret: process.env.SESSIONSECRET || 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Static Files Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+//***********************END OF YANA MIDDLEWARE
+
+// **********************NICOLE's MIDDLEWARE
+
+app.use(express.static("public-nicole"));
+
+// **********************END NICOLE's MIDDLEWARE
 
 // Middleware to parse the request body
 app.use(express.urlencoded({ extended: true }));
@@ -106,9 +180,110 @@ app.use((err, req, res, next) => {
 // Serve static files from the root directory
 app.use(express.static(path.join(__dirname, '/')));
 
-// Start the server
-sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+//****************ALEJANDRA DEFINED routes, GETS, DELETES POSTS
+
+// localhost:3001/blog will take us to /public/blog.html
+app.get("/blog", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/blog.html"))
+);
+
+// localhost:3001/api/blog will display saved blog posts JSON
+app.get("/api/blog", (req, res) => {
+    fs.readFile("./db/blog.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        const blogData = JSON.parse(data);
+        res.json(blogData);
+      });
 });
+
+//backup path. Any other get request not listed above will take user to /public/index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+//says what happens when user provides a delete request.
+app.delete("/api/blog/:id", (req, res) => {
+    //read current notes
+    fs.readFile("./db/blog.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        //save request parameter as deleteID
+        const deleteID = req.params.id;
+        //parse currentPosts string ---> JSON objest
+        let currentPosts = JSON.parse(data);
+
+        //iterate through currentPosts. Any with the same ID as requested will be spliced from the JSON file.
+        for (let i=0; i<currentPosts.length; i++) {
+            if (deleteID === currentPosts[i].id) {
+                currentPosts.splice(i, 1);
+            }
+        };
+
+        //stringify current notes JSON ---> string
+        currentPosts = JSON.stringify(currentPosts);
+
+        //provide request response to user
+        res.send("Note has been deleted");
+
+        //update current notes to blog.json file
+        fs.writeFile(`./db/blog.json`, currentPosts, (err) =>
+            err
+                ? console.error(err)
+                : console.log(
+                    "Object deleted!"
+                )
+        );
+    })
+})
+
+//says what happens when user provides a POST request
+app.post("/api/blog", (req, res) => {
+    let response;
+    //making sure post has title and text
+    if (req.body.title && req.body.text) {
+        response = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuidv4(),  //give item ID, this will help with displaying and deleting saved notes.
+            datePosted : getTimeAndDate(),
+            likes : getLikesandDislikes.randomLikes(),
+            dislikes : getLikesandDislikes.randomDislikes()
+        };
+
+        res.json("Blog post with title " + response.title + " has been added")
+
+        //check current blog.json
+        fs.readFile("./db/blog.json", "utf-8", (err, data) => {
+            if (err) throw err;
+
+            //parse current notes JSON, string ---> JSON object
+            const originalNotes = JSON.parse(data);
+            //add new note to originalNotes JSON object
+            originalNotes.push(response);
+            //stringify noteString to pass it back to blog.json, JSON ---> string
+            const noteString = JSON.stringify(originalNotes);
+
+            //write new noteString to blog.json
+            fs.writeFile(`./db/blog.json`, noteString, (err) =>
+                err
+                    ? console.error(err)
+                    : console.log(
+                        "Blog post with title " + response.title + " has been added"
+                    )
+            );
+        })
+    }
+    //in case user enters a get request that does not contain title and text
+    else {
+        res.json("Request body must contain a title and text");
+    }
+});
+
+//****************END OF ALEJANDRA DEFINED routes, GETS, DELETES POSTS
+
+
+//****************YANA START SERVER
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, () => console.log(`Now listening on port ${port}`));
+  });
+
+//****************YANA END OF START SERVER
