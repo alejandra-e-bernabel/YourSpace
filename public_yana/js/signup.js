bcc0f74e23e54a8f56d783f8d40f4f19f4001424
Alejandra <alejandra.e.bernabel@gmail.com>
2023-10-12 21:27:24 -0400

    Added all Yana files

---
+++ public_yana/js/signup.js
@@ -0,0 +1,63 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const signupForm = document.querySelector('.signup-form');
+  
+//     signupForm.addEventListener('submit', async (e) => {
+//       e.preventDefault();
+  
+//       const name = document.querySelector('#name').value;
+//       const email = document.querySelector('#email').value;
+//       const password = document.querySelector('#password').value;
+  
+//       try {
+//         const response = await fetch('/signup', {
+//           method: 'POST',
+//           body: JSON.stringify({ name, email, password }),
+//           headers: { 'Content-Type': 'application/json' },
+//         });
+  
+//         if (response.ok) {
+//           // Redirect or handle successful signup
+//           window.location.href = '/login';
+//         } else {
+//           // Handle signup failure, e.g., show an error message
+//           console.error('Signup failed');
+//         }
+//       } catch (err) {
+//         console.error('Error:', err);
+//       }
+//     });
+//   });
+  
+​
+// document.addEventListener('DOMContentLoaded', () => {
+//   const signupButton = document.querySelector('#signup-button');
+​
+//   signupButton.addEventListener('click', signupFormHandler)
+// });
+​
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+​
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+​
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+​
+  if (name && email && password) {
+    const response = await fetch('/user', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+​
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+​
