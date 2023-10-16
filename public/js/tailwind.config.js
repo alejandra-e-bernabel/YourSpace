b65a3ff3010562d870530b15d615f0421177023d
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-10-16 11:07:19 -0400

    added tailwind.config, fixed pathing and readded my build script

---
+++ public/js/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './home.html',
+      "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+      colors: {
+        'white': '',
+        'french': '#AEB3CC',
+        'marian': '#26398C',
+        'arylide': '#EAD96C',
+        'medium': '#7673BF',
+        'buff': '#D89C83',
+  
+      },
+      extend: {},
+    },
+    plugins: [
+    ],
+  }
