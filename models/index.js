e9170729935e4479dad83ef8cd399e3fef9773cc
Yana Mishyna <yana.mishina.92@gmail.com>
2023-10-17 17:48:20 -0400

    fixing a bug in login

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // collect all the individual Sequelize models representing database tables) and export them as a single object 
 // so that they can be easily imported and used in other parts of the application.
 
-const Playlist = require('./Playlist');
+const Playlist = require('./playlist');
 const User = require('./User');
 const Friend = require('./Friend');
 
